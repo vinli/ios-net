cbc20a72c741367020cd4a9e89d0ec5d7a2526eb
Jai Ghanekar <jai@vin.li>
2016-03-02 13:22:25 -0600

    Need to add post/deletes and specific object model testing

--- VinliSDKTests/VLRulesIntegrationTests.m
+++ VinliSDKTests/VLRulesIntegrationTests.m
@@ -15,8 +15,13 @@
 @property NSString *accessToken;
 @property NSDictionary *devices;
 @property VLDevice *device;
+@property NSDictionary *rules;
 
 @end
+
+
+
+//need to add methods for posting and deleting
 
 @implementation VLRulesIntegrationTests
 
@@ -51,7 +56,15 @@
     
     
     
-    
+    XCTestExpectation *expectationR = [self expectationWithDescription:@"rules call"];
+    [[VLSessionManager sharedManager].service startWithHost:self.accessToken requestUri:@"https://rules.vin.li/api/v1/devices/d47ef610-c7b9-44ac-9a41-39f9c6056de5/rules" onSuccess:^(NSDictionary *result, NSHTTPURLResponse *response) {
+        [expectationR fulfill];
+        self.rules = result;
+        XCTAssertTrue(YES);
+    } onFailure:^(NSError *error, NSHTTPURLResponse *response, NSString *msg) {
+        XCTAssertTrue(NO);
+    }];
+    [self waitForExpectationsWithTimeout:0.5 handler:nil];
     
     
 }
@@ -59,6 +72,25 @@
 - (void)tearDown {
 
     [super tearDown];
+}
+
+- (void)testGetAllRulesWithDeviceId {
+    NSDictionary *expectedJSON = self.rules;
+    XCTestExpectation *expectedRules = [self expectationWithDescription:@"rules service call"];
+    [[VLSessionManager sharedManager].service getRulesForDeviceWithId:self.device.deviceId onSuccess:^(VLRulePager *rulePager, NSHTTPURLResponse *response) {
+        [expectedRules fulfill];
+        XCTAssertEqual(rulePager.rules.count, [expectedJSON[@"rules"] count]);
+        
+        XCTAssertEqual(rulePager.total, [expectedJSON[@"meta"][@"pagination"][@"total"] unsignedLongValue]);
+        
+        //XCTAssertTrue([[[rulePager.rules objectAtIndex:0] name] isEqualToString:expectedJSON[@"rules"][0][@"name"]]);
+
+        
+        
+    } onFailure:^(NSError *error, NSHTTPURLResponse *response, NSString *bodyString) {
+        XCTAssertTrue(NO);
+    }];
+    [self waitForExpectationsWithTimeout:0.5 handler:nil];
 }
 
 
