b29b6981d84fc9b8ea1c10558eebacc92a36e788
Tommy Brown <tommy@vin.li>
2016-09-22 14:18:38 -0500

    Added support for battery status

---
+++ VinliSDK/VLBatteryStatus.h
@@ -0,0 +1,24 @@
+//
+//  VLBatteryStatus.h
+//  VinliSDK
+//
+//  Created by Tommy Brown on 9/22/16.
+//  Copyright © 2016 Vinli. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSInteger, VLBatteryStatusColor) {
+    VLBatteryStatusColorGreen,
+    VLBatteryStatusColorYellow,
+    VLBatteryStatusColorRed
+};
+
+@interface VLBatteryStatus : NSObject
+
+@property (readonly) VLBatteryStatusColor status;
+@property (strong, readonly, nonatomic) NSString *timestamp;
+
+- (instancetype) initWithDictionary:(NSDictionary *)dictionary;
+
+@end
