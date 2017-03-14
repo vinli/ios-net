e1ad9b08a3a6b94ac412237034400e7c8d3d195b
Bryan Ortiz <bryan@vin.li>
2017-03-14 16:48:09 -0500

    Made third party frameworks dynamic

---
+++ Pods/Target Support Files/jetfire/jetfire-umbrella.h
@@ -0,0 +1,10 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "JFRSecurity.h"
+#import "JFRWebSocket.h"
+
+FOUNDATION_EXPORT double jetfireVersionNumber;
+FOUNDATION_EXPORT const unsigned char jetfireVersionString[];
+
